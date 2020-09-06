ca8a50c8bb5fc40ee176bfda8cc8f7e1837f73cc
JulsKawa <juvat.2211@gmail.com>
2020-09-06 11:49:33 +0200

    Create bitcoinlucky.py

---
+++ bitcoinlucky.py
@@ -0,0 +1,23 @@
+from bitcoin import *
+import requests
+import json
+
+m = open('privkey.txt', 'a')
+def gen_priv_key():
+    my_private_key = random_key()
+    print(my_private_key)
+    my_pub_key = privkey_to_pubkey(my_private_key)
+    my_adrr = privkey_to_address(my_private_key)
+    print(my_pub_key)
+    print(my_adrr)
+    r = requests.get(f'https://chain.api.btc.com/v3/address/{my_adrr}?_ga=2.241681924.748894143.1599323586-2084259633.1599323586')
+    todos = json.loads(r.text)
+    bal = todos.get('balance')
+    if bal != None:
+        print("T'es Riche Mgl")
+        m.write(my_private_key)
+        m.write(bal)
+        m.write(my_adrr)
+        m.close()
+while True:
+    gen_priv_key()
